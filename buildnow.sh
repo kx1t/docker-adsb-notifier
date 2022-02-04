6a3c5718a3675dcec345d9545a44897a437daec6
kx1t <kx1t@amsat.org>
2022-02-04 22:22:33 +0000

    updated build script

--- buildnow.sh
+++ buildnow.sh
@@ -6,10 +6,14 @@ set -x
 [[ "$BRANCH" == "main" ]] && TAG="latest" || TAG="$BRANCH"
 [[ "$ARCHS" == "" ]] && ARCHS="linux/armhf,linux/arm64,linux/amd64"
 
+IMAGE=kx1t/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG
+echo "press enter to start building $IMAGE"
+read
+
 # rebuild the container
 pushd ~/git/docker-planefence-notifier
 git checkout $BRANCH || exit 2
 
 git pull -a
-docker buildx build --progress=plain --compress --push $2 --platform $ARCHS --tag kx1t/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG .
+docker buildx build --progress=plain --compress --push $2 --platform $ARCHS --tag $IMAGE .
 popd
