8357e80dcd7f82df52978532dffcfcd832c5ac67
kx1t <ramonkolb@gmail.com>
2022-02-10 10:39:33 -0500

    typo

--- rootfs/run/nginx/html/notify.php
+++ rootfs/run/nginx/html/notify.php
@@ -1,15 +1,17 @@
 <?php
   // concatenate the input strings into a single argument string
 
-if (isset($_GET['status']) {
+if (isset($_GET['status']))
+{
   $notif_id = escapeshellarg($_GET['status']);
 
   system("/opt/notifier/monitor.sh {$notif_id}", $return_value);
   ($return_value == 0) or die("500 php error({$return_value})");
-} else {
-
+} else
+{
   $argstring = "";
-  foreach ($_POST as $key => $value) {
+  foreach ($_POST as $key => $value)
+  {
     $key = escapeshellarg($key);
     $value = escapeshellarg($value);
     $argstring .= "{$key}={$value} ";
